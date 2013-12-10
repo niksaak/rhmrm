f8043c3f177ad8c888e2e6dddaeadeb0d299e778
Mikola Samardak <niksaak@gmail.com>
2013-12-11 01:18:39 +0200

    write parser test

---
+++ asm/parser_test.go
@@ -0,0 +1,29 @@
+package asm
+
+import "testing"
+
+// mkErrFunction returns an ErrorHandler which reports every error
+// to test runner.
+func mkErrFunction(t *testing.T) ErrorHandler {
+	return func(p Position, msg string) {
+		t.Errorf("%v: %s\n", &p, msg)
+	}
+}
+
+var asm = `
+:foo mov r9, r0    ;; full clause
+     add r1, 1eech ;; no label, suffixed hex number
+:_bar              ;; no instruction, private label
+     mtc &pc, r1   ;; prefixed control register
+`
+
+func TestParseProgram(t *testing.T) {
+	err := mkErrFunction(t)
+	l := new(Lexer).Init([]byte(asm), "", err)
+	p := new(Parser).Init(l, err)
+	program := p.ParseProgram()
+	// TODO: check resulting ast in a proper way.
+	if program == nil {
+		t.Errorf("program was not parsed")
+	}
+}
