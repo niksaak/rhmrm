a3209a00968d9c90ef84925f293f8aa5e73b6359
Mikola Samardak <niksaak@gmail.com>
2013-12-16 02:58:56 +0200

    rewrite register indexing in reginfo(), fix a bug in getBase()

--- asm/util.go
+++ asm/util.go
@@ -2,21 +2,21 @@ package asm
 
 import "strconv"
 
-import . "github.com/niksaak/rhmrm"
+import "github.com/niksaak/rhmrm/machine"
 
 var control_regs = map[string]int{
-	"pc": C_PC,
-	"ex": C_EX,
-	"ia": C_IA,
-	"im": C_IM,
-	"ir": C_IR,
-	"fl": C_FL,
+	"pc": machine.PC,
+	"ex": machine.EX,
+	"ia": machine.IA,
+	"im": machine.IM,
+	"ir": machine.IR,
+	"fl": machine.FL,
 }
 var general_regs = map[string]int{
-	"zr": R_ZR,
-	"ra": R_RA,
-	"fp": R_FP,
-	"sp": R_SP,
+	"zr": machine.ZR,
+	"ra": machine.RA,
+	"fp": machine.FP,
+	"sp": machine.SP,
 }
 
 // check register index.
@@ -47,19 +47,24 @@ func reginfo(r string) (kind, num int, ok bool) {
 	case 'r':
 		return regnchk(31, gr, n)
 	case 's':
-		return regnchk(R_S7, gr, R_S0+n)
+		return regnchk(machine.S+7, gr, machine.S+n)
 	case 't':
-		return regnchk(R_T7, gr, R_T0+n)
+		return regnchk(machine.T+7, gr, machine.T+n)
 	case 'v':
-		return regnchk(R_V3, gr, R_V0+n)
+		return regnchk(machine.V+3, gr, machine.V+n)
 	case 'a':
-		return regnchk(R_A7, gr, R_A0+n)
+		return regnchk(machine.A+7, gr, machine.A+n)
 	}
 	return 0, 0, false
 }
 
 // get base from string, return base and string without base part.
 func getBase(s string) (int, string) {
+	// strings of length 1 do not have any affixes
+	if len(s) == 1 {
+		return 10, s
+	}
+	// check for suffix
 	switch suf, end := s[len(s)-1:], len(s)-1; suf {
 	case "h", "x":
 		return 16, s[:end]
@@ -68,6 +73,7 @@ func getBase(s string) (int, string) {
 	case "b":
 		return 2, s[:end]
 	}
+	// check for prefix
 	switch pre := s[:2]; pre {
 	case "0x", "0h":
 		return 16, s[2:]
@@ -76,6 +82,7 @@ func getBase(s string) (int, string) {
 	case "0b":
 		return 2, s[2:]
 	}
+	// otherwise base is 10
 	return 10, s
 }
 
