a74860e4f391d7f7eaa00527dbdc1616aeedb246
Mikola Samardak <niksaak@gmail.com>
2013-12-19 01:10:54 +0200

    write a simple executable stub

---
+++ rhmrm.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+import (
+	"github.com/niksaak/rhmrm/asm/ast"
+	"github.com/niksaak/rhmrm/asm/compiler"
+	"github.com/niksaak/rhmrm/asm/lexer"
+	"github.com/niksaak/rhmrm/asm/parser"
+	"github.com/niksaak/rhmrm/asm/util"
+	"github.com/niksaak/rhmrm/machine"
+)
+
+var (
+	_ ast.Node
+	_ compiler.Compiler
+	_ lexer.Lexer
+	_ parser.Parser
+	_ = util.GeneralRegisterKind
+	_ machine.Machine
+)
+
+func main() {
+	fmt.Println("This executable is a linting stub")
+	fmt.Println("if you can run it, you can build rhmrm packages.")
+}
